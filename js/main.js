d5fa555761ddc03e0c549cb31ff3be3164045ff4
jaxxzer <jwalser90@gmail.com>
2016-02-01 15:29:45 -0500

    asdf

--- js/main.js
+++ js/main.js
@@ -68,14 +68,14 @@ window.onload = function() {
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
 //    	game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 200, 200, 200 );
-		if(!loose) {
+//		if(!loose) {
 		    bouncy1.x = game.input.mousePointer.x;
 		    bouncy1.y = game.input.mousePointer.y;
 		    
 		    game.physics.arcade.overlap(bouncy, bouncy1, updateText);
 		    
 		    text.setText(this.game.time.totalElapsedSeconds().toFixed(2));
-		}
+//		}
     	//updateText();
     }
     
