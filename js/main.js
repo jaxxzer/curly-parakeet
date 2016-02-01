a18a629fc51d685d9acb2f31ea2ff2ca9169a852
jaxxzer <jwalser90@gmail.com>
2016-02-01 00:23:14 -0500

    add sprites

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ window.onload = function() {
     function preload() {
         // Load an image and call it 'logo'.
         game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image( 'bomb', 'assets/bomb.png' );
+        game.load.image( 'mario', 'assets/mario.png' );
     }
     
     var bouncy;
@@ -25,9 +27,9 @@ window.onload = function() {
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
         bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        bouncy1 = game.add.sprite( game.world.leftX, game.world.centerY, 'logo' );
+        bouncy1 = game.add.sprite( game.world.leftX, game.world.centerY, 'mario' );
         bouncy2 = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        bouncy3 = game.add.sprite( game.world.centerX, game.world.bottomY, 'logo' );
+        bouncy3 = game.add.sprite( game.world.centerX, game.world.bottomY, 'bomb' );
        // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
