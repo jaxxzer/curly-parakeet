b21655e86236568749585e026c108101c12f2531
jaxxzer <jwalser90@gmail.com>
2016-02-01 00:40:02 -0500

    try add sprites

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ window.onload = function() {
        // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
-        
+        bouncy1.anchor.setTo( 0.5, 0.5 );
         // Turn on the arcade physics engine for this sprite.
         game.physics.enable( bouncy, Phaser.Physics.ARCADE );
         // Make it bounce off of the world bounds.
