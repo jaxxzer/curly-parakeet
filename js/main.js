48db6a30bf53bb207c1f31f9cf012448f3a276f8
jaxxzer <jwalser90@gmail.com>
2016-02-01 18:38:37 -0500

    game over message

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ window.onload = function() {
     }
     
     function updateText() {
-	    text.setText("Game Over!\nYou lasted: " + this.game.time.totalElapsedSeconds().toFixed(2) + " seconds!");
+	    text.setText("Game Over!\nYou lasted: ");
 
     	//text.setText("hello");
     	loose = true;
